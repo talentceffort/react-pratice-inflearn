20878189ad616e1e7f747adc6e1fce144ff6acbc
talentceffort <talentceffort@gmail.com>
2020-12-02 20:09:37 +0900

    UseImperative

---
+++ src/hook/UseImperativeHandle.tsx
@@ -0,0 +1,20 @@
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
+
+function Profile(_, ref) {
+  const [name, setName] = useState('mike')
+  const [age, setAge] = useState(0);
+
+  useImperativeHandle(ref, () => ({
+    addAge: value => setAge(age + value),
+    getNameLength: () => name.length,
+  }))
+
+  return (
+    <div>
+      <p>{`name is ${name}`}</p>
+      <p>{`age is ${age}`}</p>
+    </div>
+  )
+}
+
+export default forwardRef(Profile)
